cd44ee067755603fffbe83860107f71bc3222843
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-08-05 17:16:20 +0100

    add padding character at the end (fix)

--- app/lib/url_updater.rb
+++ app/lib/url_updater.rb
@@ -1,6 +1,6 @@
 module URLUpdater
   def url_update!
-    store_json = @store.to_json
+    store_json = "#{@store.to_json}|"
     if @store.previous_hash != "" && @store.previous_hash != store_json
       $$.location.hash   = store_json
     end
@@ -9,8 +9,12 @@ module URLUpdater
 
   def url_load_values!
     json_configs  = $$.location.hash
-    json_configs  = json_configs[1..-2].to_n
-    `console.log(window.location.hash)`
+    json_configs  = json_configs[1..-1]
+
+    # decode the url it it's CGI encoded
+    json_configs = $$.decodeURI json_configs if json_configs[0] == "%"
+
+    json_configs = json_configs[0..-2].to_n
     `json_configs = JSON.parse(json_configs)`
     configs  = Hash.new `json_configs`
     configs.each do |key, val|
